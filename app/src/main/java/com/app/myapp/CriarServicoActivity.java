4d8b51799ab5e7d9f165126c8a7f2bcb75f34204
Guilherme <guisantos86@gmail.com>
2018-05-06 19:57:52 -0300

    Cadastro de serviço:
    - Utilização do CircleImageView

--- app/src/main/java/com/app/myapp/CriarServicoActivity.java
+++ app/src/main/java/com/app/myapp/CriarServicoActivity.java
@@ -5,6 +5,7 @@ import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -92,7 +93,7 @@ public class CriarServicoActivity extends AppCompatActivity {
             if (imgNext != null)
                 imgNext.setVisibility(ImageView.VISIBLE);
         } catch (Exception e) {
-
+            Log.e("CriarServicoActivity", e.getMessage(), e);
         }
     }
 }
